3c7d60f7cb05d704353f7fb4058ffc3b1bd7fdb9
pompom.huang <pompom.huang@fuhu.com>
2014-12-03 12:16:43 +0800

    Add setter and getter

--- src/main/java/com/pompom/training/cassandra/datastaxdriver/SampleUser.java
+++ src/main/java/com/pompom/training/cassandra/datastaxdriver/SampleUser.java
@@ -2,11 +2,40 @@ package com.pompom.training.cassandra.datastaxdriver;
 
 import java.util.Set;
 
+import com.datastax.driver.mapping.annotations.ClusteringColumn;
+import com.datastax.driver.mapping.annotations.PartitionKey;
 import com.datastax.driver.mapping.annotations.Table;
 
 @Table(keyspace = "cassandra_example", name = "sample_user")
 public class SampleUser {
+	@PartitionKey(value = 0)
 	private String group;
+	@ClusteringColumn(value = 0)
 	private String name;
 	private Set<String> habit;
+
+	public String getGroup() {
+		return group;
+	}
+
+	public void setGroup(String group) {
+		this.group = group;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<String> getHabit() {
+		return habit;
+	}
+
+	public void setHabit(Set<String> habit) {
+		this.habit = habit;
+	}
+
 }
